b3dc7bd651b4bf7a37239a8be0a961ff88b36e07
Gettix <49359249+Gettix@users.noreply.github.com>
2019-05-05 13:07:19 +0700

    Update coins.js

--- cmds/coins.js
+++ cmds/coins.js
@@ -8,19 +8,19 @@ module.exports.run = async (bot, message, args) => {
       coins: 0
     };
   }
-
+  const clientem = client.emojis.get("574457191218806785");
   let uCoins = coins[message.author.id].coins;
-
+  const emojim = client.emojis.find(emoji => emoji.name === "Money");
 
   let coinEmbed = new Discord.RichEmbed()
   .setAuthor(message.author.username)
   .setColor("#00FF00")
-  .addField("<:coffee:>", uCoins);
+  .addField(`<${emojim}>`, uCoins);
 
   message.channel.send(coinEmbed).then(msg => {msg.delete(5000)});
 
 }
 
 module.exports.help = {
-  name: "coffee"
+  name: "mypixels"
 }
