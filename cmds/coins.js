58bbfbda33be3854571efad0ee479b5bb820701b
Gettix <49359249+Gettix@users.noreply.github.com>
2019-05-05 13:16:30 +0700

    Update coins.js

--- cmds/coins.js
+++ cmds/coins.js
@@ -1,4 +1,5 @@
-const client = require("discord.js");
+const Discord = require("discord.js");
+const client = new Discord.Client();
 let coins = require("../profile.json");
 
 module.exports.run = async (bot, message, args) => {
